dfd83c8443a34a3cc771f4e1fc3318dba1fcf862
AlexKing9527 <1536319510@qq.com>
2018-02-14 00:18:19 +0800

    first commit

--- JS/js1.js
+++ JS/js1.js
@@ -1,4 +1,4 @@
-    $(function () {
+$(function () {
         'use strict';
         var $add_file_task=$(".add_task");
         var $moving,$detailing,$detail_submit,$task_item,$check;
@@ -27,11 +27,44 @@
 
         function init() {
             task_list=store.get("task_list")||[];
-                loop_list();
+            if (task_list.length){
+                loop_list();}
+            time_check()
+        }
+
+        function time_check() {
+
+
+            var now_date = new Date();
+
+            now_date = now_date.getTime();
+            // console.log(now_date,task_time)
+            setInterval(function () {
+                for (var i = 0; i < task_list.length; i++) {
+                    var item = task_list[i];
+                    if (!item || !item.date || item.complete) continue;
+
+                    var task_time = new Date(item.date).getTime();
+                    if (task_time > now_date) {
+                        update_task(i,{complete:true});
+                        notify(item.value)
+                    }
+                    console.log(item);
+                }
+            },1000)
+        }
+
+
+        function notify(item) {
+            Lobibox.notify('success', {
+                size: 'mini',
+                title: item,
+                msg: 'Lorem ipsum dolor sit amet hears farmer indemnity inherent.'
+            });
         }
 
         function join_list(new_task,index) { //单个list模板
-            if (!new_task || index=="undefined") return;
+            if (!new_task || index== undefined) return;
             var temple =
                 '<div class="task_list" data-index="'+index+'">'+
                 //checkbox加入判断，当result为true时，打勾
@@ -59,7 +92,7 @@
             // console.log("task_list",task_list);
             for (var i=0;i<task_list.length;i++){
                 var item=task_list[i];
-                if (item.result) {
+                if (item && item.result) {
                     complete_item[i]=item;
                     }
                 else {
@@ -67,7 +100,7 @@
                      // console.log(2,task_list);
                      $(".main").prepend($task)}}
 
-                console.log(complete_item);
+                console.log("complete_item",complete_item);
 
             for (var j=0;j<complete_item.length;j++){
                 $task=$(join_list(complete_item[j],j));//在传入task_list的同时传入index数据
@@ -101,9 +134,9 @@
             $moving.on("click",function () {
                 //提取属性data-index中的id
                 var index=$(this).parent().parent().data("index");
-                var a = confirm("是否要删除？");
-
-                a ? delete_task(index) : null
+                my_alert("是否要删除？").then(function (r) {
+                    r ? delete_task(index) : null
+                });
             });
         }
 
@@ -128,7 +161,8 @@
 
 
         $(".task_detail_mask").on("click",function () {
-            hide_mask()
+            hide_mask();
+
         });
 
         //查看详情
@@ -206,5 +240,93 @@
             })
         }
 
+        function my_alert(text) {
+
+            var conf = {},
+                $yes,
+                $no,
+                $dfd,
+                confirmed,
+                $my_alert_body;
+
+            $dfd = $.Deferred();
+
+            //判断传入参数是否能作为标题，可能为字符类或对象
+            $my_alert_body = $('<div class="my_alert">'+
+                '<div id="top_alert"></div>'+
+                '<div id="bottom_alert"><button class="alert_yes" style="margin-right: 15px">确定</button>'+
+                '<button class="alert_no">取消</button></div>'+
+                '</div>');
+
+            $("body").append($my_alert_body);
+
+            $yes=$(".my_alert").find(".alert_yes");
+            $no=$(".my_alert").find(".alert_no");
+
+            if (!text) {
+                console.error("alert需要标题")
+            }
+            if (typeof text == "string") {
+                conf = text;
+            }
+            else {
+                conf = $.extend(conf, text);
+            }
+
+            $("#top_alert").append(conf);
+
+            adjust_my_alert();
+
+            //根据浏览器宽度动态居中
+            function adjust_my_alert() {
+
+                var $window_width,
+                    $window_height,
+                    $box_width,
+                    $box_height,
+                    a,
+                    b;
+                $window_width=$(window).width();
+                $window_height=$(window).height();
+                $box_width=$my_alert_body.width();
+                $box_height=$my_alert_body.height();
+                // console.log($window_width,$window_height,$box_width,$box_height)
+                a=($window_width-$box_width)/2;
+                b=($window_height-$box_height)/2;
+                console.log(a,b);
+                $my_alert_body.css({
+                    left:a,
+                    top:b-100 //位置屏幕中间偏上
+                });
+            }
+
+            //当浏览器窗口变化时再执行窗口位置设定模块
+            $(window).on("resize",function () {
+                adjust_my_alert()
+            });
+
+            var timer1 = setInterval(function () {
+                //当confirm被赋值后，传出参数
+                if (confirmed !== undefined){
+                    $dfd.resolve(confirmed);
+                    clearInterval(timer1);
+                    close_myalert()
+                }
+            },50);
+
+            $yes.on('click',function () {
+                confirmed=true;
+            });
+            $no.on('click',function () {
+                confirmed=false;
+            });
+
+            return $dfd.promise();
+
+            function close_myalert() {
+                $my_alert_body.remove();
+            }
+
+        }
 
     });
